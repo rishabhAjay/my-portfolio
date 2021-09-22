2b240497c36824e2c1d7966742edd30c5d913cc0
rishabhAjay <rishabhajay24@gmail.com>
2021-09-22 17:32:18 +0530

    v 1.4

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,10 @@ const Home = () => {
           content="A portfolio website built with Next.js and Material UI"
         />
 
-        <meta property="linkedin:image" content="/favicon.png" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/1m1Hx98/Logopit-1632309910245.jpg"
+        />
 
         <meta property="og:site_name" content="Rishabh Ajay's Homepage" />
         <link rel="icon" href="/favicon.png" />
