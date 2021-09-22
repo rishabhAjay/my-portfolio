24940edc3cb1a9e58180e8d45368b4669d8e3bec
Rishabh Ajay <63163183+rishabhAjay@users.noreply.github.com>
2021-09-22 15:16:48 +0530

    v 1.3

--- components/Experience.js
+++ components/Experience.js
@@ -20,16 +20,16 @@ const Experience = () => {
       date: "2015-2016",
     },
     {
-      title: "Manasarowar Pushkarini Vidyashrama",
-      location: "Mysore, India",
-      designation: "12th Grade",
-      date: "2017-2018",
+       title: "Our Own English High School",
+      location: "Sharjah, UAE",
+      designation: "10th Grade",
+      date: "2015-2016",
     },
     {
-      title: "Vidyavardhaka College of Engineering",
-      location: "Mysore, India",
-      designation: "B.E CSE",
-      date: "2018-2022",
+      title: "Our Own English High School",
+      location: "Sharjah, UAE",
+      designation: "10th Grade",
+      date: "2015-2016",
     },
   ]);
   useEffect(() => {
