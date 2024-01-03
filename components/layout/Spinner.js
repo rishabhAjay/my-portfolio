c0360a12580ab549a5470c9cad0839fef1c83e4f
rishabhAjay <rishabhajay24@gmail.com>
2024-01-03 12:27:12 +0530

    removed unnecessary css from spinner

--- components/layout/Spinner.js
+++ components/layout/Spinner.js
@@ -2,16 +2,7 @@ import { Grid } from "react-loader-spinner";
 const Spinner = ({ color, size, style }) => {
   return (
     <div className={style}>
-      <Grid
-        visible={true}
-        height={size}
-        width={size}
-        color={color}
-        ariaLabel="grid-loading"
-        radius="12.5"
-        wrapperStyle={{}}
-        wrapperClass="grid-wrapper"
-      />
+      <Grid height={size} width={size} color={color} />
     </div>
   );
 };
